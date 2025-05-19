d48613bdf577a7df2de8bbe6e69f2092b9d92698
quasar-vortex <162831819+quasar-vortex@users.noreply.github.com>
2025-05-19 15:57:24 -0700

    Update backup_db.sh

--- backup_db.sh
+++ backup_db.sh
@@ -2,11 +2,27 @@
 
 out_dir="/opt/backups"
 cur_date=$(date +'%Y-%m-%d_%H-%M')
+sql_file="$cur_date.sql"
+compressed="$sql_file.bz2"
+bkp_dir="$out_dir/bkps"
 
-mkdir -p "$out_dir/bkps"
-if [ -s "$out_dir/$cur_date.sql.bz2" ]; then
-	mv "$out_dir/$cur_date.sql.bz2" "$out_dir/bkps/$cur_date.sql.bz2.bkp"
-fi
+function handle_existing_items() {
+	mkdir -p "$bkp_dir"
+	if [ -s "$sql_file" ]; then
+		echo "$sql_file already exists. Exiting, wait to backup at least one minute."
+		exit 1
+	fi
+	if [ -s  "$compessed" ]; then
+		echo "Moving $compressed to $bkp_dir/$compressed.bkp"
+		mv "$compressed" "$bkp_dir/$compressed.bkp"
+	fi
+}
 
-mysqldump portfolio > "$out_dir/$cur_date.sql"
-bzip2 "$out_dir/$cur_date.sql
+function dump_file() {
+	echo "Dumping SQL File and Compressing"
+	mysqldump portfolio > "$sql_file"
+	bzip2 "$sql_file"
+}
+
+handle_existing_items
+dump_file
